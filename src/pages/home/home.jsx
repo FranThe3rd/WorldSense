c25949024475d712e4fdc81c32bcb18b599bff97
Francisco <figueroaf@moravian.edu>
2025-12-19 00:06:27 -0500

    ok homepage most likely done, gonna work on some media queries next

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -17,7 +17,7 @@ export const Home = () => {
 
 
     return (
-        <div className='Home'>
+        <div className='home'>
             <div className='home-text-div'>
                 <motion.h1 
                 
@@ -26,24 +26,37 @@ export const Home = () => {
                 animate={{y:0,opacity: 1}}
                 transition={{duration:0.5 }}
                 >World Sense</motion.h1>
-                <motion.p className='caption1'>Discover information about the world that is not known</motion.p>
-                <p className='caption2'>
+                <motion.p className='caption1'
+                initial={{y:-10, opacity:0}}
+                animate={{y:0,opacity: 1}}
+                transition={{duration:0.5,delay:0.5 }}
+
+                >Discover information about the world that is not known</motion.p>
+                <motion.p className='caption2'
+                 initial={{y:-10, opacity:0}}
+                animate={{y:0,opacity: 1}}
+                transition={{duration:0.5,delay:0.7 }}
+                >
                     World Sense is an interactive, map-based platform that brings together public data from multiple trusted sources. Explore FBI Most Wanted listings, crime statistics, and other real-world information, all visualized on a single interactive map to help you better understand what’s happening around you in real time.
-                </p>
-                <div className='home-button-div'>
+                </motion.p>
+                <motion.div className='home-button-div'
+                
+                 initial={{y:-10, opacity:0}}
+                animate={{y:0,opacity: 1}}
+                transition={{duration:0.5,delay:0.9 }}>
                     <motion.button
                         className='get-started-btn'
-                        whileHover={{ scale: 1.2 }}
+                        whileHover={{ scale: 1.1 }}
                         whileTap={{ scale: 0.8 }}
 
                     >Get Started</motion.button>
                     <motion.button
                         className='signup-btn'
-                        whileHover={{ scale: 1.2 }}
+                        whileHover={{ scale: 1.1 }}
                         whileTap={{ scale: 0.8 }}
 
                     >Sign Up</motion.button>
-                </div>
+                </motion.div>
 
 
             </div>
