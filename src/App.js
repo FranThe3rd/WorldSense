24c4f157dffd20dc7ab5c789038a914af501179a
FranThe3rd <francisco21xd234@gmail.com>
2025-12-20 21:51:14 -0500

    started working on navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/home';
-
+import Navbar from './components/Navbar/Navbar.jsx'
 function App() {
   return (
     <Router>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar() {
+
+  return (
+  <div className="Navbar">Test </div>
+  );
+}
+
+export default Navbar;
